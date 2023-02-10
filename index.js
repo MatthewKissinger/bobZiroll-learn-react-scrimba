aab4a1bff1ba229f248bbf93ef35c4a4248f6916
Matt <matthew.t.kissinger@gmail.com>
2023-02-09 17:14:21 -0800

    video #20

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 
 
+function Header() {
+    return (
+        <header>
+            <nav>
+            <img src='./react-logo.png' width='40px'></img>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            </nav>
+        </header> 
+    )
+}
 
+function Footer() {
+    return (
+        <footer>
+            <small>&copy; 2023 Kissinger development. All rights reserved.</small>
+        </footer>
+    )
+}
 
-ReactDOM.render(
-    <nav className="navbar">
-        <h1>Matt's Market</h1>
-        <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>,
-    document.getElementById('root')
-);
+function MainContent() {
+    return (
+        <div>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));
+
