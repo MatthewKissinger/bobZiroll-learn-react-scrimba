9acf6d5ab09e3409219d2fb9e5d31af42835cde7
Matt <matthew.t.kissinger@gmail.com>
2023-02-11 10:28:32 -0800

    button component finished

--- digital-business-card/src/components/About.js
+++ digital-business-card/src/components/About.js
@@ -1,7 +1,8 @@
 export default function About() {
     return (
-        <div>
-            <h4>About section goes here</h4>
+        <div className="about--container">
+            <h3 className="about--title">About</h3>
+            <p className="about--text">I am a frontend developer with a particular interest in UI and user experience. I am always looking to learn new technologies and best practices.</p>
         </div>
     )
 }
--- digital-business-card/src/components/ContactBtns.js
+++ digital-business-card/src/components/ContactBtns.js
@@ -1,14 +1,16 @@
+import emailIcon from '../images/email-Icon.png';
+import linkedinIcon from '../images/linkedin-Icon.png';
 
 function ContactBtns() {
     return (
         <div className="contactBtns--container">
             <button className="contact--email-btn">
-                <img alt="email icon"></img>
+                <img alt="email icon" src={emailIcon}></img>
                 <h3>Email</h3>
             </button>
             <button className="contact--linkedin-btn">
-                <img alt="linkedin icon"></img>
-                <h3>linkedIn</h3>
+                <img alt="linkedin icon" src={linkedinIcon}></img>
+                <h3>LinkedIn</h3>
             </button>
         </div>
     )
