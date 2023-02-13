ec4f5a048f417f1ed73c5ac6e54427c6a1505f68
Matt <matthew.t.kissinger@gmail.com>
2023-02-13 10:43:38 -0800

    card component styled

---
+++ airbnb-clone/src/components/Card.js
@@ -0,0 +1,17 @@
+import katieImg from '../images/katie.png';
+import starIcon from '../images/star-icon.png';
+
+export default function Card() {
+    return (
+        <div className="card--container">   
+            <img className='card--image' src={katieImg} alt="Katie Zaferes"></img>
+            <div className='card--rating-container'>
+                <img className='card--star' src={starIcon} alt='star icon'></img>
+                <p className='card--rating-text'>5.0 <span className='card--rating-text-gray'>(6) &#x2022; USA</span></p>
+            </div>
+            <p className='card--title'>Life lessons with Katie Zaferes</p>  
+            <p className='card--price'><b>From $136</b> / person</p>
+            
+        </div>
+    )
+}
