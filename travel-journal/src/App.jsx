36365c0fdd308fdb8d1ac64edefc4b520d9a70ac
Matt <matthew.t.kissinger@gmail.com>
2023-02-17 21:29:27 -0800

    finished with card component and styling

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+import cardData from '../src/data'
 import Header from './components/Header'
 import Card from './components/Card'
 
 function App() {
+  const cards = cardData.map(item => {
+    return (
+      <div>
+        <Card 
+        key={item.id}
+        {...item}
+      />
+      <hr className='card--line--break'></hr>
+      </div>
+      
+    )
+  });
+
   return (
     <div className="app-container">
       <Header />
-      <Card />
+      <div className='cards--list'>
+        {cards}
+      </div>
     </div>  
   )
 }
--- travel-journal/src/components/Card.jsx
+++ travel-journal/src/components/Card.jsx
@@ -1,7 +1,22 @@
-export default function Card() {
+import mapPinIcon from "../assets/images/map_pin_icon.png"
+
+export default function Card(props) {
     return (
         <div className="card--container">
-            Card goes here
+            <img className="card--image" src={props.imageUrl}></img>
+            <div className="card--info--container">
+                <div className="card--location--container">
+                    <img className="card--map-icon" src={mapPinIcon}></img>
+                    <p className="card--location">{props.location}</p>
+                    <p className="card--maps-link">View on Google Maps</p>
+                </div>
+                <h2 className="card--title">{props.title}</h2>
+                <div className="card--dates">
+                    <p className="card--start-date">{props.startDate}</p> - 
+                    <p className="card--end-date">{props.endDate}</p>
+                </div>
+                <p className="card--description">{props.description}</p>
+            </div>
         </div>
     )
 }
