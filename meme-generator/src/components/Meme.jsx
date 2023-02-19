32a9c2aa9324bb5fd90e77850e6f071b414a1797
Matt <matthew.t.kissinger@gmail.com>
2023-02-19 08:10:52 -0800

    adding eventListeners to Meme.jsx

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,15 +1,35 @@
-
+import memeData from '../memesData'
 
 export default function Meme() {
+    const memesArray = memeData.data.memes;
+
+    function getRandomUrl() {
+        const randomNum = Math.floor(Math.random() * 100);
+        console.log(memesArray[randomNum].url);
+    }
+
     return (
         <div className="meme--container">
-            <input className="meme--top-input" type="text" />
-            <input className="meme--bottom-input" type="text" />
-            <button>Get a new meme image
-                <span class="material-symbols-outlined meme--button-image">
-                image
-                </span>
-            </button>
+            <div className="meme--form">
+                <input 
+                    className="meme--top-input" 
+                    type="text" 
+                    placeholder="top text"
+                />
+                <input 
+                    className="meme--bottom-input" 
+                    type="text" 
+                    placeholder="bottom text"
+                />
+                <button 
+                    onClick={getRandomUrl}
+                >
+                    Get a new meme image
+                    <span className="material-symbols-outlined meme--button-image">
+                    image
+                    </span>
+                </button>
+            </div> 
         </div>
     )
 }
