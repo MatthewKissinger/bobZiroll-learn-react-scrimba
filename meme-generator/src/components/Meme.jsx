1ebdaa87e7ee9fb07e7544c3c0bd630b67fcd9d7
Matt <matthew.t.kissinger@gmail.com>
2023-02-24 09:54:18 -0800

    updated meme state to an object

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import memeData from '../memesData';
 
 export default function Meme() {
-    const memesArray = memeData.data.memes;
 
-    const [memeImage, setMemeImage] = React.useState("");
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memeData);
+
+    // const [memeImage, setMemeImage] = React.useState("");
 
     function getMemeImage() {
+        const memesArray = allMemeImages.data.memes;
         const randomNum = Math.floor(Math.random() * memesArray.length);
         const url = memesArray[randomNum].url;
 
-        setMemeImage(url);
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: url
+            }
+        });
     }
 
     return (
@@ -35,7 +48,10 @@ export default function Meme() {
                     </span>
                 </button>
             </div> 
-            <img className='meme--image' src={memeImage}></img>
+            <img 
+                className='meme--image' 
+                src={meme.randomImage}>
+            </img>
         </div>
     )
 }
